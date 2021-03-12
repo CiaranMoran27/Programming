a3e50e1e951d56515190d9b3ddb5e4a2d550957b
unknown <G00398269@gmit.ie>
2021-03-12 03:02:25 +0000

    Week 07 Task upgrade2

--- Week07-files/Week_07_Weekly_Task/es.py
+++ Week07-files/Week_07_Weekly_Task/es.py
@@ -6,20 +6,17 @@
 import os
 import sys
 
-filename = sys.argv[1]
+filename = sys.argv[1]                             # accept the 2nd arguement from the command line
 
 def readFile(filename):
-    try:
-        with open(filename,'r') as f:           # open file in read only mode, use of alias means no need to close file
-            data = f.read()                   # call the read function on the alias
-            return data
-
-    except IOError:                        # if error encountered the user input-file does not exist
-        print("This file does not exist")
+    with open(filename,'r') as f:                     # open file in read only mode, use of alias means no need to close file
+        data = f.read()                                  # call the read function on the alias
+        return data
+        
 
 
 def eCharacterCount(data):
-    eCount = data.count("e")
+    eCount = data.count("e")                       # use of .count method to count occurances of a character in a file               
     ECount = data.count("E")
     eTotal = eCount + ECount
 
@@ -36,7 +33,7 @@ eCharacterCount(data)
 # main reference list is in pands-problem-sheet reposotory README file
 #Refernces used: 
 # 1.Docs.python.org, 2021, 7.2. Reading and Writing Files — Python 3.9.2 Documentation, viewed 05 March 2021, https://docs.python.org/3/tutorial/inputoutput.html
-# 3.GeeksforGeeks, 2019, How to use sys.argv in Python, viewed 12 March 2021, https://www.geeksforgeeks.org/how-to-use-sys-argv-in-python/.
+# 3.GeeksforGeeks, 2019, How to use sys.argv in Python, viewed 12 March 2021, https://www.geeksforgeeks.org/how-to-use-sys-argv-in-python/
 # 3.GeeksforGeeks, 2019, With statement in Python, viewed 05 March 2021, https://www.geeksforgeeks.org/with-statement-in-python.
 # 4.Gutenberg.org, 2000, Moby Dick, viewed 05 March 2021, https://www.gutenberg.org/files/2701/old/moby10b.txt
 # 5.Zíka, H, 2018, Counting specific characters in a file (Python), viewed 12 Mar 2021, https://stackoverflow.com/questions/48885930/counting-specific-characters-in-a-file-python.
